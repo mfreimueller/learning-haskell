a8ddb46a6351425bfb94bc24d3f860c930b700ad
Michael Freimüller <michael@mfreimueller.com>
2025-09-28 11:51:42 +0200

    implement improved fibonacci algorithm that relies only on one recursive function call

--- fibonacci.hs
+++ fibonacci.hs
@@ -5,9 +5,22 @@ fib n
     | n == 2 = 1
     | otherwise = fib (n - 1) + fib (n - 2)
 
+superFib :: Int -> Int
+superFib n
+    | n == 0 = 0
+    | n == 1 = 1
+    | n == 2 = 1
+    | otherwise = 1 + optFib (n - 1) 0 1
+    where
+        optFib :: Int -> Int -> Int -> Int
+        optFib n a b
+            | n <= 0 = 0
+            | otherwise = a + optFib (n - 1) b (a + b)
+
 main :: IO()
 main = do
     putStrLn "Which fibonacci number should be calculated? "
     input_n <- getLine
     let n = (read input_n :: Int)
-    print (show (fib n))
+    -- print (show (fib n))
+    print (show (superFib n))
