a017382865af447a19657ea784aa0b98f7f7f785
Michael Freimüller <michael@mfreimueller.com>
2025-10-04 13:31:16 +0200

    implement challenge 3.6.4 from "The Art of Functional Programming"

---
+++ twice.hs
@@ -0,0 +1,14 @@
+-- The Art of Functional Programming
+-- 3.6.4 Chapter 4: Apply a function twice
+
+inc :: Int -> Int
+inc x = x + 1
+
+twice :: (Int -> Int) -> Int -> Int
+twice fun val = fun (fun val)
+
+main :: IO()
+main = do
+    print (twice inc 0)
+    print (twice inc 1)
+    print (twice inc 2)
